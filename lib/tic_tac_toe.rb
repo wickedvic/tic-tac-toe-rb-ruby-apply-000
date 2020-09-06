85fb7e8b618cd9a8c76aae1cf7fba450446a1f77
Vivek Sanghvi <viksipart@gmail.com>
2020-09-06 22:25:18 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -65,7 +65,7 @@ def won?(board)
   WIN_COMBINATIONS.detect do |combo|
     if board[combo[0]] == board[combo[1]] &&
       board[combo[1]] == board[combo[2]] &&
-      
+      position_taken?(board, combo[0])
     
   end
   
